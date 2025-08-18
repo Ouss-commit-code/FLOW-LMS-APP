99a246d1b64e334d44a4d342f533ac8bfdd919a8
@Ouss-commit <oussamajeffal@gmail.com>
2025-08-18 05:21:30 +0100

    update Course Grid

---
+++ main/FLOW/web-ed/components/CourseCard.tsx
@@ -0,0 +1,31 @@
+import { GetCoursesQueryResult } from "@/sanity.types";
+import Link from "next/link";
+
+interface CourseCardProps {
+  course: GetCoursesQueryResult[number];
+  progress?: number;
+  href: string;
+}
+
+function CourseCard({ course, progress, href }: CourseCardProps) {
+  return ( 
+    <Link href={href}
+     prefetch={false}
+     className="group hover:no-underline flex"
+    >
+    <div className="bg-card rounded-xl overflow-hidden shadow-lg
+     transition-all duration-300 ease-in-out
+      hover:shadow-xl hover:translate-y-[-4px] border border-border 
+      flex flex-col flex-1">
+        test
+    
+    </div>    
+    
+
+
+  </Link>
+  );
+  
+}
+
+export default CourseCard;
